b613b044eda7174e8bc3387e6a6cdab70ed84186
Matteo Chauvin <229449054+chauvinmatteo@users.noreply.github.com>
2026-03-09 15:33:39 +0100

    feat(a_maze_ing): updated main with parsing

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,12 +1,28 @@
 from mazegen import MazeGenerator, Algorithm
+from parser import parsed_info
 import sys
 
 
 def main() -> None:
-
-    maze: MazeGenerator = MazeGenerator(10, 10, (0, 0), (0, 0))
+    file: sys.TextIOWrapper = open("config.txt")
+    data: dict = parsed_info(file)
+    if "WIDTH" in data.keys():
+        width: int = int(data.get("WIDTH"))
+    if "HEIGHT" in data.keys():
+        height: int = int(data.get("HEIGHT"))
+    if "ENTRY" in data.keys():
+        entries: tuple = data.get("ENTRY").split(",")
+        first_entry = int(entries[0])
+        second_entry = int(entries[1])
+        entry: tuple[int, int] = (first_entry, second_entry)
+    if "EXIT" in data.keys():
+        exits: tuple = data.get("EXIT").split(",")
+        first_exit = int(exits[0])
+        second_exit = int(exits[1])
+        exit: tuple[int, int] = (first_exit, second_exit)
+    maze: MazeGenerator = MazeGenerator(width, height, entry, exit)
     sys.setrecursionlimit(1000000)
-    print(maze.generate(Algorithm.DFS))
+    print(maze.generate(Algorithm.HAK))
     print(maze.visualize())
 
 
