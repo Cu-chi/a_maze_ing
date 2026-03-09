67d6ff1f4ec66186d73fcbfd54e87699c0ef5673
Matteo Chauvin <mchauvin@z4r3p5.42lyon.fr>
2026-03-09 09:14:29 +0100

    dev(mazegen/algorithm): HAK algorithm working

--- mazegen/algorithms/hak.py
+++ mazegen/algorithms/hak.py
@@ -12,18 +12,19 @@ class HAK(BaseAlgorithm):
         self.visited: bool = [[False for _ in range(self.w)]
                               for _ in range(self.h)]
 
-    def walk(self, cx: int, cy: int) -> grid[int] | None:
-        self.visited[cx][cy] = True
+    def walk(self, cy: int, cx: int) -> tuple[int, int] | None:
+        self.visited[cy][cx] = True
         directions: list[int] = [self.N, self.S, self.E, self.W]
         random.shuffle(directions)
         for dir in directions:
             nx: int = cx + self.DX[dir]
             ny: int = cy + self.DY[dir]
             if (0 <= ny < len(self.grid)) and (0 <= nx < len(self.grid[ny])) \
-               and not self.visited[nx][ny]:
-                self.grid[cx][cy] |= dir
-                self.grid[nx][ny] |= self.OPPOSITE[dir]
-                return [nx, ny]
+               and not self.visited[ny][nx]:
+                self.grid[cy][cx] |= dir
+                self.grid[ny][nx] |= self.OPPOSITE[dir]
+                return (ny, nx)
+        return None
 
     def hunt(self) -> Optional[tuple[int, int]]:
         for cy in range(self.h):
@@ -34,17 +35,20 @@ class HAK(BaseAlgorithm):
                         ny: int = cy + self.DY[direction]
                         if (0 <= ny < len(self.grid)) and \
                             (0 <= nx < len(self.grid[ny])) \
-                                and self.visited[nx][ny]:
-                            self.grid[cx][cy] |= dir
-                            self.grid[nx][ny] |= self.OPPOSITE[dir]
-                            self.visited[cx][cy] = True
-                            return (cx, cy)
-            return None
+                                and self.visited[ny][nx]:
+                            self.grid[cy][cx] |= direction
+                            self.grid[ny][nx] |= self.OPPOSITE[direction]
+                            self.visited[cy][cx] = True
+                            return (cy, cx)
+        return None
 
     def generate(self) -> grid:
-        current_cords = (0, 0)
-        while current_cords:
-            cx, cy = current_cords
-            self.walk(cx, cy)
-            current_cords = self.hunt()
-        return (self.grid)
+        current: tuple = (0, 0)
+        while current:
+            while current:
+                next: grid[list[int]] | None = self.walk(*current)
+                if not next:
+                    break
+                current = next
+            current = self.hunt()
+        return self.grid
