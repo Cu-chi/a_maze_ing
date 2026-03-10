aeeb905433d168c51a4f1d99ad08a616342735b6
Matteo Chauvin <mchauvin@z3r12p2.42lyon.fr>
2026-03-10 10:34:20 +0100

    tweak(a_maze_ing): first error handling in main

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -6,24 +6,40 @@ import sys
 def main() -> None:
     file: sys.TextIOWrapper = open("config.txt")
     data: dict = parsed_info(file)
-    if "WIDTH" in data.keys():
-        width: int = int(data.get("WIDTH"))
-    if "HEIGHT" in data.keys():
-        height: int = int(data.get("HEIGHT"))
-    if "ENTRY" in data.keys():
-        entries: tuple = data.get("ENTRY").split(",")
-        first_entry = int(entries[0])
-        second_entry = int(entries[1])
-        entry: tuple[int, int] = (first_entry, second_entry)
-    if "EXIT" in data.keys():
-        exits: tuple = data.get("EXIT").split(",")
-        first_exit = int(exits[0])
-        second_exit = int(exits[1])
-        exit: tuple[int, int] = (first_exit, second_exit)
+    try:
+        if "WIDTH" in data.keys():
+            width: int = int(data.get("WIDTH"))
+    except ValueError:
+        print("Error: width has to be a integer.")
+        return
+    try:
+        if "HEIGHT" in data.keys():
+            height: int = int(data.get("HEIGHT"))
+    except ValueError:
+        print("Error: height has to be a integer.")
+        return
+    try:
+        if "ENTRY" in data.keys():
+            entries: tuple = data.get("ENTRY").split(",")
+            first_entry = int(entries[0])
+            second_entry = int(entries[1])
+            entry: tuple[int, int] = (first_entry, second_entry)
+    except ValueError:
+        print("Error: entry has to be a tuple of integers.")
+        return
+    try:
+        if "EXIT" in data.keys():
+            exits: tuple = data.get("EXIT").split(",")
+            first_exit = int(exits[0])
+            second_exit = int(exits[1])
+            exit: tuple[int, int] = (first_exit, second_exit)
+    except ValueError:
+        print("Error: exit has to be a tuple of integers.")
+        return
     maze: MazeGenerator = MazeGenerator(width, height, entry, exit)
     sys.setrecursionlimit(1000000)
     maze.draw_42()
-    print(maze.generate(Algorithm.DFS))
+    print(maze.generate(Algorithm.HAK))
     print(maze.visualize())
 
 
