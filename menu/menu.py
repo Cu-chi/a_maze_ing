48af621e5dbca67d48026c30b79cc37987e8b303
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-03-10 14:42:23 +0100

    feat(menu): handle inputs and better visualization

---
+++ menu/menu.py
@@ -0,0 +1,53 @@
+from pynput import keyboard
+from typing import Any, Callable
+import functools
+import sys
+
+
+class Menu:
+    def __init__(self, menu: list[tuple[str, Callable[[], Any]]]) -> None:
+        self.current_indexed: int = 0
+        self.exiting: bool = False
+        self.menu: list[tuple[str, Callable[[], Any]]] = menu
+        self.listener: keyboard.Listener = keyboard.Listener(
+            on_press=functools.partial(Menu.handle_keyboard_press, self)
+        )
+        self.listener.start()
+
+    @staticmethod
+    def set_cursor_position(x: int, y: int) -> None:
+        sys.stdout.write(f"\033[{x};{y}H")
+
+    @staticmethod
+    def show(output: str) -> None:
+        Menu.set_cursor_position(0, 0)
+        sys.stdout.write(output)
+        sys.stdout.write("\033[?25l")
+        sys.stdout.flush()
+
+    def append_menu(self, output: str) -> str:
+        output += "\n"
+        for i, menu_data in enumerate(self.menu):
+            if self.current_indexed == i:
+                output += "\033[47m\033[30m"
+            output += f"{i}: " + menu_data[0] + "\033[0m\n"
+        return output
+
+    def handle_keyboard_enter(self) -> None:
+        self.menu[self.current_indexed][1]()
+
+    def handle_keyboard_press(self,
+                              key: keyboard.Key |
+                              keyboard.KeyCode |
+                              None) -> None:
+        try:
+            if keyboard.Key.up == key:
+                if self.current_indexed > 0:
+                    self.current_indexed -= 1
+            if keyboard.Key.down == key:
+                if self.current_indexed < len(self.menu) - 1:
+                    self.current_indexed += 1
+            if keyboard.Key.enter == key:
+                self.handle_keyboard_enter()
+        except Exception:
+            pass
