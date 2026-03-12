41e87bc50fbb56cfa85baf2b3534641759022b9a
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-03-12 11:36:45 +0100

    refactor(algorithms): made a class for directions so it's reusable

--- mazegen/algorithms/dfs.py
+++ mazegen/algorithms/dfs.py
@@ -1,20 +1,21 @@
 from mazegen.algorithms.base import BaseAlgorithm
-from mazegen.utils import grid
+from mazegen.utils import grid, Directions as Dir
 import random
 
 
 class DFS(BaseAlgorithm):
     def dfs(self, cx: int, cy: int) -> None:
-        directions: list[int] = [self.N, self.S, self.E, self.W]
+        directions: list[int] = [Dir.N, Dir.S,
+                                 Dir.E, Dir.W]
         random.shuffle(directions)
         for dir in directions:
-            nx: int = cx + self.DX[dir]
-            ny: int = cy + self.DY[dir]
+            nx: int = cx + Dir.DX[dir]
+            ny: int = cy + Dir.DY[dir]
 
             if (0 <= ny < len(self.grid)) and (0 <= nx < len(self.grid[ny])) \
                and self.grid[ny][nx] == 0:
                 self.grid[cy][cx] = self.grid[cy][cx] | dir
-                self.grid[ny][nx] = self.grid[ny][nx] | self.OPPOSITE[dir]
+                self.grid[ny][nx] = self.grid[ny][nx] | Dir.OPPOSITE[dir]
                 self.dfs(nx, ny)
 
     def generate(self) -> grid:
--- mazegen/algorithms/hak.py
+++ mazegen/algorithms/hak.py
@@ -1,6 +1,5 @@
 from mazegen.algorithms.base import BaseAlgorithm
-from mazegen.utils import grid, point
-# from typing import Optional
+from mazegen.utils import grid, point, Directions as Dir
 import random
 
 
@@ -14,15 +13,15 @@ class HAK(BaseAlgorithm):
 
     def walk(self, cy: int, cx: int) -> point | None:
         self.visited[cy][cx] = True
-        directions: list[int] = [self.N, self.S, self.E, self.W]
+        directions: list[int] = [Dir.N, Dir.S, Dir.E, Dir.W]
         random.shuffle(directions)
         for dir in directions:
-            nx: int = cx + self.DX[dir]
-            ny: int = cy + self.DY[dir]
+            nx: int = cx + Dir.DX[dir]
+            ny: int = cy + Dir.DY[dir]
             if (0 <= ny < len(self.grid)) and (0 <= nx < len(self.grid[ny])) \
                and not self.visited[ny][nx] and self.grid[ny][nx] != -1:
                 self.grid[cy][cx] |= dir
-                self.grid[ny][nx] |= self.OPPOSITE[dir]
+                self.grid[ny][nx] |= Dir.OPPOSITE[dir]
                 return (ny, nx)
         return None
 
@@ -30,14 +29,14 @@ class HAK(BaseAlgorithm):
         for cy in range(self.h):
             for cx in range(self.w):
                 if not self.visited[cy][cx] and self.grid[cy][cx] != -1:
-                    for direction in [self.N, self.S, self.E, self.W]:
-                        nx: int = cx + self.DX[direction]
-                        ny: int = cy + self.DY[direction]
+                    for direction in [Dir.N, Dir.S, Dir.E, Dir.W]:
+                        nx: int = cx + Dir.DX[direction]
+                        ny: int = cy + Dir.DY[direction]
                         if (0 <= ny < len(self.grid)) and \
                             (0 <= nx < len(self.grid[ny])) \
                                 and self.visited[ny][nx]:
                             self.grid[cy][cx] |= direction
-                            self.grid[ny][nx] |= self.OPPOSITE[direction]
+                            self.grid[ny][nx] |= Dir.OPPOSITE[direction]
                             self.visited[cy][cx] = True
                             return (cy, cx)
         return None
