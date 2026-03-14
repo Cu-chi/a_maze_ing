f48f55712a1078a8c47cab31a8b62bc7444bd35a
Matteo Chauvin <mchauvin@z1r11p2.42lyon.fr>
2026-03-14 10:45:10 +0100

    tweak(parsing): added two functions to get data

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 from io import TextIOWrapper
+from mazegen.utils import point
 
 
 def parsed_info(file: TextIOWrapper) -> dict[str, str]:
@@ -9,3 +10,23 @@ def parsed_info(file: TextIOWrapper) -> dict[str, str]:
         value: str = data[1].strip("\n").strip("\"")
         config.update({key: value})
     return config
+
+
+def get_int(data: dict[str, str], key: str) -> int:
+    if key not in data:
+        raise KeyError(f"missing key : {key}")
+    try:
+        return int(data[key])
+    except ValueError:
+        raise ValueError(f"{key.lower()} has to be an integers!")
+
+
+def get_point(data: dict[str, str], key: str) -> point:
+    if key not in data:
+        raise KeyError(f"missing key : {key}")
+    try:
+        parts: list[str] = data[key].split(",")
+        return int(parts[0]), int(parts[1])
+    except (ValueError, IndexError):
+        raise ValueError(f"{key.lower()} has to be a tuple of two "
+                         "integers (x,y).")
