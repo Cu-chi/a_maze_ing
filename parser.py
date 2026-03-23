88cc9efa7d05f05bda4747bb42e5cc3063b101aa
Matteo Chauvin <mchauvin@z2r2p4.42lyon.fr>
2026-03-23 15:45:14 +0100

    refactor(parsing): added perfect flag and error handling

--- parser.py
+++ parser.py
@@ -2,12 +2,21 @@ from io import TextIOWrapper
 from mazegen.utils import point
 
 
+class ConfigError(Exception):
+    pass
+
 def parsed_info(file: TextIOWrapper) -> dict[str, str]:
     config: dict[str, str] = {}
     for line in file:
         data: list[str] = line.split("=")
+        if line == "\n":
+            raise ConfigError("blank line in config.txt")
+        if len(data) < 2:
+            raise ConfigError("the line doesn't respect key=value")
         key: str = data[0].strip(" ")
         value: str = data[1].strip("\n").strip("\"")
+        if len(value) < 1:
+            raise ConfigError("the line doesn't respect key=value")
         config.update({key: value})
     return config
 
@@ -30,3 +39,16 @@ def get_point(data: dict[str, str], key: str) -> point:
     except (ValueError, IndexError):
         raise ValueError(f"{key.lower()} has to be a tuple of two "
                          "integers (x,y).")
+
+
+def get_bool(data: dict[str, str], key: str) -> bool:
+    value: str | None = data.get(key)
+    clean_value: str = value.strip().lower()
+    if key not in data:
+        raise KeyError(f"missing key : {key}")
+
+    if clean_value == "true":
+        return True
+    if clean_value == "false":
+        return False
+    raise ValueError(f"{key.lower()} has to be a boolean (True or False).")
